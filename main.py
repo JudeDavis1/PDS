3a72ab1ae46439f22816c8a753070a9c319c14c1
Jude Davis <judedavis92@gmail.com>
2022-10-14 21:29:08 +0100

    Added support for already generated CSV files for flexibility.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import csv
 import bs4
 import sys
@@ -8,7 +9,7 @@ from typing import List
 
 def main():
     print('[*] Fetching data...')
-    p = People()
+    p = People(postcode='SL2')
 
     p.get_phone_numbers()
     print('[*] Saving results...')
@@ -26,44 +27,83 @@ class Field:
 
 class People:
 
-    def __init__(self, name='Smith', postcode='SL1'):
-        self.name = name
-        self.postcode = postcode
-        self.link_classes = 'ml-3 d-inline light-blue my-auto no-wrap'
-        self.url = f'https://www.thephonebook.bt.com/Person/PersonSearch/?Surname={self.name}&Location={self.postcode}'
+	def __init__(self, name='Smith', postcode='SL1', filename='phone_number_fields.csv'):
+		self.name = name
+		self.postcode = postcode
+		self.link_classes = 'ml-3 d-inline light-blue my-auto no-wrap'
+		self.url = f'https://www.thephonebook.bt.com/Person/PersonSearch/?Surname={self.name}&Location={self.postcode}'
 
-        self.html = requests.get(self.url).text
-        self.soup = bs4.BeautifulSoup(self.html, 'html.parser')
-        self.fields = [Field.serial, Field.name, Field.number, Field.postcode, Field.is_completed]
+		self.html = requests.get(self.url).text
+		self.soup = bs4.BeautifulSoup(self.html, 'html.parser')
+		self.fields = [Field.serial, Field.name, Field.number, Field.postcode, Field.is_completed]
 
-        try:
-            self.fileobj = open('phone_number_fields.csv', 'w+', newline='')
-        except PermissionError:
-            print('[-] Error! Could not open Excel file. Please try closing any programs that are using the file, then rerun the program...')
-            sys.exit(-1)
-        self.writer = csv.DictWriter(self.fileobj, self.fields)
+		self.starting_sn = 0
+		self.numbers_to_add = []
+		self.explored_numbers = set()
+		self._updates: List[dict] = []  # Cannot directly update the writer, so save for the future
 
-    def get_phone_numbers(self, max=10) -> List[str]:
-        self.numbers = []
-        links = self.soup.find_all(class_=self.link_classes)
+		try:
+			if os.path.exists(filename):
+				self.fileobj_r = open(filename, 'r', newline='')
+				self.reader = csv.DictReader(self.fileobj_r)
 
-        for i in range(len(links)):
-            self.numbers.append(links[i].text.strip('\n'))
+				print('[*] Copying previous data...')
 
-        return self.numbers
+				for row in self.reader:
+					self._updates.append({
+						Field.serial: row[Field.serial],
+						Field.name: row[Field.name],
+						Field.number: row[Field.number],
+						Field.postcode: row[Field.postcode],
+						Field.is_completed: row[Field.is_completed]
+					})
+					self.explored_numbers.add(row[Field.number])
+					self.starting_sn = int(row[Field.serial]) + 1
 
-    def save(self):
-        self.writer.writeheader()
+				self.fileobj_r.close()
 
-        for i in range(len(self.numbers)):
-            self.writer.writerow({
-                Field.serial: str(i),
-                Field.name: self.name,
-                Field.number: self.numbers[i],
-                Field.postcode: self.postcode,
-                Field.is_completed: 'No'
-            })
+			self.fileobj_w = open(filename, 'w', newline='')
+			self.writer = csv.DictWriter(self.fileobj_w, self.fields)
+		except PermissionError:
+			print('[-] Error! Could not open Excel file. Please try closing any programs that are using the file, then rerun the program...')
+			sys.exit(-1)
+
+	def get_phone_numbers(self, max=10) -> List[str]:
+		links = self.soup.find_all(class_=self.link_classes)
+
+		for i in range(len(links)):
+			n = links[i].text.strip('\n')
+
+			self.numbers_to_add.append(n)
+
+		return self.numbers_to_add
+
+	def save(self):
+		self.writer.writeheader()
+		if len(self._updates) != 0:
+			self._updates.sort(key=self._updates_sort_key)  # 'Yes' at the bottom, 'No' at the top
+			self.writer.writerows(self._updates)
+
+		sn = self.starting_sn
+		for i in range(len(self.numbers_to_add)):
+			if self.numbers_to_add[i] not in self.explored_numbers:  # Remove duplicate phone numbers
+				self.writer.writerow({
+					Field.serial: str(sn),
+					Field.name: self.name,
+					Field.number: self.numbers_to_add[i],
+					Field.postcode: self.postcode,
+					Field.is_completed: 'No'
+				})
+				sn += 1
+
+		self.fileobj_w.close()
+
+	def _updates_sort_key(self, x: dict):
+		if x[Field.is_completed].lower() in ['yes', 'completed', '1', 'true', 'positive']:
+			return 1
+		else:
+			return 0
 
 
 if __name__ == '__main__':
-    main()
+    main();os.system('pause')
